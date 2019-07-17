f4ad074d7eaf5c153cdf708ea393eb8beca5c593
Rana Umer <umer7866rana@gmail.com>
2019-07-18 01:31:41 +0300

    create form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,22 @@
 class ArticlesController < ApplicationController
+
+	def show
+		@article = Article.find(params[:id])
+	end
+	
 	def new
+	end
+
+	def create
+		# render plain: params[:article].inspect
+		@article = Article.new(params[:article])
+ 
+  		@article.save
+  		redirect_to @article
+
+  	private
+  		def article_params
+    		params.require(:article).permit(:name, :age, :contact, :message)
+  		end
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_165439) do
+ActiveRecord::Schema.define(version: 2019_07_17_205753) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
+    t.string "name"
+    t.string "age"
+    t.string "contact"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
