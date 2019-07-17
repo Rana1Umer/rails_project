768555c9d01f2eac22aa086fea49e242fbb54693
Rana Umer <umer7866rana@gmail.com>
2019-07-18 01:54:38 +0300

    Show_form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,20 +3,21 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
-	
+
 	def new
 	end
 
 	def create
+		byebug
 		# render plain: params[:article].inspect
-		@article = Article.new(params[:article])
+		@article = Article.new(article_params)
  
   		@article.save
   		redirect_to @article
+  	end
 
   	private
   		def article_params
     		params.require(:article).permit(:name, :age, :contact, :message)
   		end
-	end
 end
