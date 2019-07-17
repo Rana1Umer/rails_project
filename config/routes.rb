218885dabe5c51afe7fe2182c8f9f153d8e01c7f
Rana Umer <umer7866rana@gmail.com>
2019-07-17 23:42:14 +0300

    First Task

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/contact'
   get 'welcome/about_us'
+
+  root 'welcome#index'
+
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
